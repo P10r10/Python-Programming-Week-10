dbc5f84ee870aa770d8ed40b266d6bca4fb55e95
P10r10 <asantiago@keemail.me>
2023-06-29 15:02:25 +0100

    course records

--- course_records.py
+++ course_records.py
@@ -20,11 +20,13 @@ class Menu:
                 break
             self.__treat_option(option)
 
-    def __treat_option(self, option: int):
+    def __treat_option(self, option: int) -> None:
         if option == "1":
             self.__add_course()
         elif option == "2":
-            self.__get_course_data()
+            self.__show_course_data()
+        elif option == "3":
+            self.__statistics()
 
     def __add_course(self):
         name = input("course: ")
@@ -32,13 +34,16 @@ class Menu:
         credits = int(input("credits: "))
         self.__studies.add_course(Course(name, grade, credits))
 
-    def __get_course_data(self):
+    def __show_course_data(self) -> None:
         name = input("course: ")
         course = self.__studies.get_course(name)
         if course is None:
             print("no entry for this course")
         else:
             print(course)
+
+    def __statistics(self) -> None:
+        Statistics(self.__studies).show()
 
 
 class Course:
@@ -47,8 +52,14 @@ class Course:
         self.__grade = grade
         self.__credits = credits
 
-    def name(self):
+    def name(self) -> str:
         return self.__name
+
+    def grade(self) -> int:
+        return self.__grade
+
+    def credits(self) -> int:
+        return self.__credits
 
     def __str__(self) -> str:
         return f"{self.__name} ({self.__credits} cr) grade {self.__grade}"
@@ -58,15 +69,47 @@ class Studies:
     def __init__(self) -> None:
         self.__courses = {}
 
-    def add_course(self, course: Course):
-        self.__courses[course.name()] = course
+    def add_course(self, course: Course) -> None:
+        if not course.name() in self.__courses:
+            self.__courses[course.name()] = course
+        elif self.__courses[course.name()].grade() < course.grade():
+            self.__courses[course.name()] = course
 
     def get_course(self, name: str) -> Course:
         if name not in self.__courses:
             return None
         return self.__courses[name]
 
+    def get_courses(self) -> dict:
+        return self.__courses
+
+    def courses_with_grade(self, grade: int) -> int:
+        return sum([c.grade() == grade for c in self.__courses.values()])
+
+
+class Statistics:
+    def __init__(self, studies: Studies) -> None:
+        self.__studies = studies
+
+    def __get_total_credits(self):
+        return sum([c.credits() for c in self.__studies.get_courses().values()])
+
+    def __get_mean_grade(self, nb_courses: int) -> float:
+        if nb_courses == 0:
+            return 0
+        grades = [c.grade() for c in self.__studies.get_courses().values()]
+        return sum(grades) / nb_courses
+
+    def show(self):
+        nb = len(self.__studies.get_courses())  # total number of courses
+        credits = self.__get_total_credits()
+        mean = self.__get_mean_grade(nb)
+        print(f"{nb} completed courses, a total of {credits} credits")
+        print(f"mean {mean:.1f}")
+        print("grade distribution")
+        for i in range(5, 0, -1):
+            print(f"{i}: {self.__studies.courses_with_grade(i) * 'x'}")
+
 
 if __name__ == "__main__":
     Menu().show()
-    # TODO statistics create Statistics class in: Studies
